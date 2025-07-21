7f049663cf353d6361f4b0c29285b7db6ea448ac
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-22 02:22:59 +0600

    initial setup

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("from signup");
+};
+export const login = (req, res) => {
+  res.send("from login");
+};
+export const logout = (req, res) => {
+  res.send("from logout");
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 
-
+//middlewares
 const app  = express();
 dotenv.config();
+app.use(express.json());
 
 
+//routes
+app.use('/api/auth', authRouter);
+
+//server running
 app.listen(process.env.PORT, ()=>{
     console.log('Server Running at port: 3000');
 })
