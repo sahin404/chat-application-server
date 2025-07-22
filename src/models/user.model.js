a259cd34ab7000b614070d173a4b05eaa6087bd0
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-22 12:04:13 +0600

    create user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        require:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        minlegth:6
+    },
+    profilePic:{
+        type:String,
+        default:"",
+    },
+},{
+    timestamps:true,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
