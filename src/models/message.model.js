db9a8de2d60d5c41905adc92fadb007e4021257a
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-24 23:41:24 +0600

    model create

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -2,7 +2,22 @@ import mongoose from "mongoose";
 
 const messageSchema = mongoose.Schema(
     {
-
+        senderId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        receiverId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required: true
+        },
+        text:{
+            type:String
+        },
+        image:{
+            type:String
+        }        
     }, { timestamps:true }
 );
 
