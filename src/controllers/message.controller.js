16e5fb9371b44d8239c29efb75909dd107bd2d0e
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-24 23:52:42 +0600

    message related all routes setup

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,3 +1,11 @@
-export const getMessage=async(req,res)=>{
+export const getUsers = async (req, res) => {
+
+};
+
+export const getMessage = async (req, res) => {
+
+};
+
+export const sendMessage = async (req, res) => {
     
-}
+};
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { getMessage, getUsersForSideBar, sendMessage } from '../controllers/message.controller';
 
 const messageRouter = express.Router();
 
+messageRouter.get('/users', getUsersForSideBar);
+messageRouter.get('/get/:id', getMessage);
+messageRouter.post('/send/:id', sendMessage);
 
 
 export default messageRouter;
