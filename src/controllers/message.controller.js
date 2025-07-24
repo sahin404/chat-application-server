29eef5181a13627ca9136bc059ebff16839d2bf5
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-25 00:30:23 +0600

    getusers for side bar

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,11 +1,19 @@
-export const getUsers = async (req, res) => {
+import User from "../models/user.model";
+
+export const getUsersForSideBar = async (req, res) => {
+
+  try {
+    const myId = req.user._id;
+    const users = await User.find({ _id: { $ne: myId } }); //ne=>not equal
+    res.staus(200).json(users);
+  } 
+  catch (err) {
+    console.error("Sidebar user fetch error:", err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 
 };
 
-export const getMessage = async (req, res) => {
+export const getMessage = async (req, res) => {};
 
-};
-
-export const sendMessage = async (req, res) => {
-    
-};
+export const sendMessage = async (req, res) => {};
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getMessage, getUsersForSideBar, sendMessage } from '../controllers/message.controller';
+import { getMessage, getUsersForSideBar, sendMessage } from '../controllers/message.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const messageRouter = express.Router();
 
-messageRouter.get('/users', getUsersForSideBar);
-messageRouter.get('/get/:id', getMessage);
-messageRouter.post('/send/:id', sendMessage);
+messageRouter.get('/users', protectRoute, getUsersForSideBar);
+messageRouter.get('/get/:id', protectRoute, getMessage);
+messageRouter.post('/send/:id', protectRoute, sendMessage);
 
 
 export default messageRouter;
