135bee1c190379823c44246f12910cd6fbac0d59
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-26 01:21:11 +0600

    update

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js';
 import { dbConnect } from './libs/db.js';
 import messageRouter from './routes/message.route.js';
+import cors from 'cors';
 
 //middlewares
 const app  = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 //routes
 app.use('/api/auth', authRouter);
