ad94cdbff435f1b605e603a2e4cfefe0a550d6f3
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-30 03:24:35 +0600

    socket io setup

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import authRouter from './routes/auth.route.js';
 import { dbConnect } from './libs/db.js';
 import messageRouter from './routes/message.route.js';
 import cors from 'cors';
+import {app,server} from './libs/socket.js';
 
 //middlewares
-const app  = express();
 dotenv.config();
 // app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
@@ -23,7 +23,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
 
 //server running
-app.listen(process.env.PORT, ()=>{
+server.listen(process.env.PORT, ()=>{
     console.log('Server Running at port: 3000');
     dbConnect();
 })
