61c63152f66532215ddbbb2616f32e3b5f476ff3
Md. Sahin Alan <sahinraj20@gmail.com>
2025-10-02 23:25:42 +0600

    updated the database credentials

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173', //connect with client
   credentials: true
 }));
 
